51fdbf7c8484c7bb62954819ea169ea9cee89b17
Shadowpapa <jm_matheus_20@hotmail.com>
2020-06-25 17:26:57 -0300

    Add project files.

---
+++ Triangulo/ClasseTriangulo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangulo
+{
+    class ClasseTriangulo
+    {
+        public double A;
+        public double B;
+        public double C;
+    }
+}
